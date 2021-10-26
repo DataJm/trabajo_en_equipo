efdb88f89fde9db507eda5ba86825487daf37372
JM <jm@192.168.1.9>
2021-10-25 19:52:23 -0500

    config

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # trabajo_en_equipo
 Nuestro proyect ofinal
+
+# How to use
+Necesitas un config.py en la raiz del proyecto donde venga:
+```
+gkey = "your api key"
+otra_api = "abc"
+```
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+# Necesitamos un archivo config.py donde venga
+# la llave de la api
+try:
+    import gkey from config
+except:
+    print("Necesitas un archivo config.py con tu gkey")
+
 datos = pd.DraFrame([1,2,3,4,5])
 
 datos.to_csv("data_previa.csv")
